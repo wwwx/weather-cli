c43d376968eb6802d214f793fb6d5b80ef4eb49e
Wang Xu <834166254@qq.com>
2022-12-10 15:52:51 +0800

    init project

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+// const { program } = require("commander");
+import { program } from "commander";
+
+program.option("-c, --city [name]", "Add city name");
+program.parse();
+
+const options = program.opts();
+
+if (process.argv.slice(2).length === 0) {
+  program.outputHelp();
+  process.exit();
+}
+
+console.log(options.city);
